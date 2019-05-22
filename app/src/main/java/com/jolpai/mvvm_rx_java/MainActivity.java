2fde2d710ec7f8425bd0e8265d143b32d44c2abd
abc <tanim2reja@gmail.com>
2019-05-22 16:13:27 +0600

    add rxjava

--- app/src/main/java/com/jolpai/mvvm_rx_java/MainActivity.java
+++ app/src/main/java/com/jolpai/mvvm_rx_java/MainActivity.java
@@ -1,16 +1,23 @@
 package com.jolpai.mvvm_rx_java;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+
+import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
+    //https://www.toptal.com/android/functional-reactive-android-rxjava
     @BindView(R.id.hello_text)
     TextView helloText;
 
@@ -27,5 +34,34 @@ public class MainActivity extends AppCompatActivity {
                 helloText.setText(s);
             }
         });
+
+        //Creating a observable object which will emit 1,2,3 respectively
+        Observable <Integer>  observable = Observable.just(1,2,3);
+
+        observable.subscribe(new Observer<Integer>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("Test", "In onError()");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d("Test", "In onNext():" + integer);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("Test", "In onCompleted()");
+            }
+        });
+
+
+
     }
 }
